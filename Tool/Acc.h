a7c3b5e80f2bcb44ce7e30fc2d03e8476af4860b
Koushik <koushik.m@niser.ac.in>
2015-03-11 04:25:38 -0500

    first commit

---
+++ Tool/Acc.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include "TH1.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TFile.h"
+#include "TLorentzVector.h"
+#include "TVector3.h"
+#include "TChain.h"
+#include "TMath.h"
+#include "TString.h"
+#include "SusyAnaTools/Tools/NTupleReader.h"
+
+bool FillChain(TChain *chain, const TString &inputFileList)
+{
+  ifstream infile(inputFileList, ifstream::in);
+  std::string buffer;
+  if(!infile.is_open())
+    {
+      std::cerr << "** ERROR: Can't open '" << inputFileList << "' for input" << std::endl;
+      return false;
+    }
+  std::cout << "TreeUtilities : FillChain " << std::endl;
+  while(1)
+    {
+      infile >> buffer;
+      if(!infile.good()) break;
+      //std::cout << "Adding tree from " << buffer.c_str() << std::endl;
+      chain->Add(buffer.c_str());
+    }
+  std::cout << "No. of Entries in this tree : " << chain->GetEntries() << std::endl;
+  return true;
+}

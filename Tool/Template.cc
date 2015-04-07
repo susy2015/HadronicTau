9bfb6062ee2cee6196910291533b581085418dd5
Koushik <koushik.m@niser.ac.in>
2015-04-07 10:00:10 -0500

    Changed jet matching criterion

--- Tool/Template.cc
+++ Tool/Template.cc
@@ -72,30 +72,33 @@ int main(int argc, char* argv[]) {
     std::vector<double>jetspt = tr.getVec<double>("jetspt");
     std::vector<double>jetseta = tr.getVec<double>("jetseta");
     std::vector<double>jetsphi = tr.getVec<double>("jetsphi");
-    std::vector<double>gentaupt = tr.getVec<double>("gentaupt");
-    std::vector<double>gentaueta = tr.getVec<double>("gentaueta");
-    std::vector<double>gentauphi = tr.getVec<double>("gentauphi");
+    std::vector<double>genhadtaupt = tr.getVec<double>("genhadtaupt");
+    std::vector<double>genhadtaueta = tr.getVec<double>("genhadtaueta");
+    std::vector<double>genhadtauphi = tr.getVec<double>("genhadtauphi");
+    std::vector<double>genvisiblehadtaueta = tr.getVec<double>("genvisiblehadtaueta");
+    std::vector<double>genvisiblehadtauphi = tr.getVec<double>("genvisiblehadtauphi");
     int hadtauflag = tr.getVar<int>("hadtauflag");
 
     // Select only events where the W decayed into a hadronically decaying tau
-    if( !(hadtauflag == 1) ) continue;
+        if( !(hadtauflag == 1) ) continue;
+        if(genhadtaupt.size()==0)continue;//To skip rare events where same tau decays both into hadron and lepton!
+
     // Kinematic variables of generator-level tau
-    const double genTauPt = gentaupt.at(0);
-    const double genTauEta = gentaueta.at(0);
-    const double genTauPhi = gentauphi.at(0);
+    const double genTauPt = genhadtaupt.at(0);
+    const double genTauEta = genhadtaueta.at(0);
+    const double genTauPhi = genhadtauphi.at(0);
+    const double genvisibleTauEta = genvisiblehadtaueta.at(0);
+    const double genvisibleTauPhi = genvisiblehadtauphi.at(0);
 
     // Use only events where the tau is inside the muon acceptance (pt>20, eta>2.1)because lateron we will apply the response to muon+jet events
     if( genTauPt < TauResponse::ptMin() ) continue;
     if( fabs(genTauEta) > TauResponse::etaMax() ) continue;
 
-
-
-
     // Do the matching
     int tauJetIdx = -1;		// Will store the index of the jet matched to the tau
     const float deltaRMax = genTauPt < 50. ? 0.2 : 0.1; // Increase deltaRMax at low pt to maintain high-enought matching efficiency
     const unsigned nObj = jetspt.size();
-    if( !utils::findMatchedObject(tauJetIdx,genTauEta,genTauPhi,jetseta,jetsphi,nObj,deltaRMax) ) continue;
+    if( !utils::findMatchedObject(tauJetIdx,genvisibleTauEta,genvisibleTauPhi,jetseta,jetsphi,nObj,deltaRMax) ) continue;
     
 
     // Fill histogram with relative visible energy of the tau

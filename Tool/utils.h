0646f1ec23881c059a2ce16caea2684ced96cb6b
Koushik <koushik.m@niser.ac.in>
2015-04-17 10:38:25 -0500

    two jet matching function included

--- Tool/utils.h
+++ Tool/utils.h
@@ -21,7 +21,7 @@ namespace utils {
   //  - false : otherwise. In that case, 'matchedObjIdx == -1'
   // If deltaRMax is specified, it is in addition required  that 
   // the objects are closer than deltaRMax.
-  bool findMatchedObject(int &matchedObjIdx, double eta, double phi, std::vector<double> jetseta, std::vector<double> jetsphi, unsigned nObj, float deltaRMax) {
+  bool findTauMatchedObject(int &matchedObjIdx, double eta, double phi, std::vector<double> jetseta, std::vector<double> jetsphi, unsigned nObj, float deltaRMax) {
     matchedObjIdx = -1;
     double deltaRMin = 100000.;
     for(unsigned objIdx = 0; objIdx < nObj; ++objIdx) { // Loop over objects
@@ -42,6 +42,20 @@ namespace utils {
     return match;
   }
 
+  void findMuMatchedObject(int &matchedObjIdx, double eta, double phi, std::vector<double> jetseta, std::vector<double> jetsphi, unsigned nObj,float deltaRMax) {
+    matchedObjIdx = -1;
+    double deltaRMin = 100000.;
+    for(unsigned objIdx = 0; objIdx < nObj; ++objIdx) { // Loop over objects                                                                  
+      const double dr = deltaR(eta,jetseta.at(objIdx),phi,jetsphi.at(objIdx));
+      if( dr < deltaRMin ) {
+        deltaRMin = dr;
+        matchedObjIdx = objIdx;
+      }
+    } // End of loop over objects                                                                                                             
+    if( deltaRMin > deltaRMax ) {
+      matchedObjIdx = -1;
+    }
+  }
 
   double mt(double pt, double phi, double met, double metPhi) {
     const double dPhi = deltaPhi(phi,metPhi);                                

129f2a403bb3f5e7c82a508f566f1fbe95d66519
Koushik <koushik.m@niser.ac.in>
2015-04-17 08:09:53 -0500

    bug fixed

--- Tool/Acc.cc
+++ Tool/Acc.cc
@@ -121,6 +121,7 @@ int main(int argc, char* argv[]) {
     vector<int> genDecayIdxVec = tr.getVec<int>("genDecayIdxVec");
     vector<int> genDecayPdgIdVec = tr.getVec<int>("genDecayPdgIdVec");
     vector<TLorentzVector> jetsLVec = tr.getVec<TLorentzVector>("jetsLVec");
+    vector<double> recoJetsBtag_0 = tr.getVec<double>("recoJetsBtag_0");
     double met=tr.getVar<double>("met");
     double metphi=tr.getVar<double>("metphi");
 
@@ -144,8 +145,7 @@ int main(int argc, char* argv[]) {
       if(fabs(pdgId)==11) nele++;
     }
 
-    if(nmu ==1 && nele==0){//control sample
-      
+    if(nmu ==1 && nele==0){//control sample    
       const double muEta = genmueta.at(0);
       const double muPhi = genmuphi.at(0);
       const double muPt = genmupt.at(0);
@@ -163,9 +163,14 @@ int main(int argc, char* argv[]) {
       int muJetIdx = -1;
       const float deltaRMax = 0.2;
       const unsigned nObj = jetsLVec.size();
-      if( !utils::findMatchedObject(muJetIdx,muEta,muPhi,jetseta,jetsphi,nObj,deltaRMax) ) continue;
+      utils::findMuMatchedObject(muJetIdx,muEta,muPhi,jetseta,jetsphi,nObj,deltaRMax);
+
+      double cleanmetX =0;
+      double cleanmetY =0;
       vector<TLorentzVector> cleanJetVec;
+      vector<double>cleanJetBTag;
       cleanJetVec.clear();
+      cleanJetBTag.clear();
       for(int jetIdx = 0; jetIdx < jetsLVec.size(); ++jetIdx) { // Loop over reco jets
 	// Skip this jet if it is the muon
 	if( jetIdx == muJetIdx ) continue;
@@ -176,7 +181,12 @@ int main(int argc, char* argv[]) {
 	double M = jetsLVec.at(jetIdx).M();
 	TLorentzVector cleanLVec; cleanLVec.SetPtEtaPhiM(Pt, Eta, Phi, M);
 	cleanJetVec.push_back(cleanLVec);
+	cleanJetBTag.push_back(recoJetsBtag_0.at(jetIdx));
       }
+
+      //calculate met
+      cleanmetX = met*cos(metphi)+ muPt*cos(muPhi);
+      cleanmetY = met*sin(metphi)+ muPt*sin(muPhi);
 
       // Get random number from tau-response template
       const double scale = tauResp.getRandom(muPt);
@@ -188,15 +198,23 @@ int main(int argc, char* argv[]) {
       simJetVec.clear();
       TLorentzVector simLVec; simLVec.SetPtEtaPhiM(simJetPt, muEta, muPhi, muM);
       simJetVec.push_back(simLVec);
-
       vector<TLorentzVector> combJetVec;
       combJetVec.clear();
       combJetVec = combjet(cleanJetVec, simJetVec);
+      vector<double> combJetsBtag_0;
+      combJetsBtag_0.clear();
+      combJetsBtag_0 = combJetBtag(cleanJetBTag, cleanJetVec, simJetVec);
 
       //recompute met
-      double simmet = met + muPt - simJetPt;
+      double combmetX = cleanmetX;
+      double combmetY = cleanmetY;
+      combmetX -= simJetPt*cos(muPhi);
+      combmetY -= simJetPt*sin(muPhi);
+     const double combmet = sqrt(combmetX*combmetX + combmetY*combmetY);
+     const double combmetPhi = std::atan2(combmetY,combmetX);
+
       bool passmet = true;
-      if(simmet<AnaConsts::defaultMETcut) passmet = false; 
+      if(combmet<AnaConsts::defaultMETcut) passmet = false; 
 
       //recompute jet
       int nJet = AnaFunctions::countJets(combJetVec, AnaConsts::pt30Eta24Arr);
@@ -206,23 +224,25 @@ int main(int argc, char* argv[]) {
 
       //recompute deltaphi
       std::vector<double> * deltaPhiVec = new std::vector<double>();
-      (*deltaPhiVec) = AnaFunctions::calcDPhi(combJetVec, metphi, 3, AnaConsts::dphiArr);
+      (*deltaPhiVec) = AnaFunctions::calcDPhi(combJetVec, combmetPhi, 3, AnaConsts::dphiArr);
       bool passdeltaPhi = true;
       if( deltaPhiVec->at(0) < AnaConsts::dPhi0_CUT || deltaPhiVec->at(1) < AnaConsts::dPhi1_CUT || deltaPhiVec->at(2) < AnaConsts::dPhi2_CUT){
 	passdeltaPhi = false;
       }
+
       //recompute bjet
-      int cnt1CSVS = AnaFunctions::countCSVS(combJetVec, tr.getVec<double>("recoJetsBtag_0"), AnaConsts::cutCSVS, AnaConsts::bTagArr);
+      int cnt1CSVS = AnaFunctions::countCSVS(combJetVec, combJetsBtag_0, AnaConsts::cutCSVS, AnaConsts::bTagArr);
       bool passbJets = true;
       if( !( (AnaConsts::low_nJetsSelBtagged == -1 || cnt1CSVS >= AnaConsts::low_nJetsSelBtagged) && (AnaConsts::high_nJetsSelBtagged == -1 || cnt1CSVS < AnaConsts::high_nJetsSelBtagged ) ) ){
 	passbJets = false;
       }
 
       //top tagger input
-      TLorentzVector metLVec_acc; metLVec_acc.SetPtEtaPhiM(met, 0, metphi, 0);
+      TLorentzVector metLVec_acc; metLVec_acc.SetPtEtaPhiM(combmet, 0, combmetPhi, 0);
       int comb30_acc = AnaFunctions::countJets(combJetVec, AnaConsts::pt30Arr);
       std::vector<TLorentzVector> *jetsLVec_forTagger_acc = new std::vector<TLorentzVector>(); std::vector<double> *recoJetsBtag_forTagger_acc = new std::vector<double>();
-      AnaFunctions::prepareJetsForTagger(combJetVec, tr.getVec<double>("recoJetsBtag_0"), (*jetsLVec_forTagger_acc), (*recoJetsBtag_forTagger_acc));
+      AnaFunctions::prepareJetsForTagger(combJetVec, combJetsBtag_0, (*jetsLVec_forTagger_acc), (*recoJetsBtag_forTagger_acc));
+      
       int bestTopJetIdx_acc = -1;
       bool remainPassCSVS_acc = false;
       int pickedRemainingCombfatJetIdx_acc = -1;
@@ -253,7 +273,7 @@ int main(int argc, char* argv[]) {
       if(!passTopTagger) continue;
 
       genmu++;
-      if(simJetPt>20 && fabs(muEta)<2.4)accmu++;
+      if(muPt>20 && fabs(muEta)<2.4)accmu++;
     }
 
   }
@@ -266,7 +286,7 @@ int main(int argc, char* argv[]) {
 vector<TLorentzVector> combjet (const vector<TLorentzVector> &seljet, const vector<TLorentzVector> &simjet){
   vector<TLorentzVector> combNJet;
   combNJet.clear();
-  unsigned int idx;
+  unsigned int idx = seljet.size();
   for(unsigned int i=0; i<seljet.size(); i++){
     TLorentzVector comb;
     if(seljet.at(i).Pt()>simjet.at(0).Pt()){
@@ -293,4 +313,28 @@ vector<TLorentzVector> combjet (const vector<TLorentzVector> &seljet, const vect
     combNJet.push_back(comb);
   }
   return combNJet;
+}
+vector<double> combJetBtag(const vector<double> &cleanJetBtag, const vector<TLorentzVector> &seljet, const vector<TLorentzVector> &simjet){
+  vector<double>combJetBTag;
+  combJetBTag.clear();
+  unsigned int idx = cleanJetBtag.size();
+  for(unsigned int i=0; i<cleanJetBtag.size(); i++){
+    if(seljet.at(i).Pt()>simjet.at(0).Pt()){
+      combJetBTag.push_back(cleanJetBtag.at(i));
+    }
+    else{
+      combJetBTag.push_back(0);
+      idx = i;
+      break;    
+    }
+  }
+  if(idx<cleanJetBtag.size()){
+    for(unsigned int j=idx; j<cleanJetBtag.size(); j++){
+      combJetBTag.push_back(cleanJetBtag.at(j));
+    }
+  }
+    if(combJetBTag.size()==cleanJetBtag.size()){
+      combJetBTag.push_back(0);
+    }
+    return combJetBTag;
 }

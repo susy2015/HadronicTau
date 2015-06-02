b3aec20f2f24980f129562bf1744598c67bbb550
Koushik <koushik.m@niser.ac.in>
2015-06-02 02:10:11 -0500

    Bjetmistag

--- Tool/Closure.h
+++ Tool/Closure.h
@@ -44,7 +44,7 @@ void BaseHistgram::BookHistgram(const char *outFileName)
   oFile = new TFile(outFileName, "recreate");
   hPredHt = new TH1D("hPredHt",title+";H_{T} [GeV];Events",25,200.,2000.);
   hPredHt->Sumw2();
-  hPredmet = new TH1D("hPredmet",title+";met [GeV];Events",50,200.,1200.);
+  hPredmet = new TH1D("hPredmet",title+";met [GeV];Events",24,200.,800.);
   hPredmet->Sumw2();
   hPredNJets = new TH1D("hPredNJets",title+";N_{jets};Events",10,4,14);
   hPredNJets->Sumw2();
@@ -52,14 +52,14 @@ void BaseHistgram::BookHistgram(const char *outFileName)
   hPredNbJets->Sumw2();
   hPredNTops = new TH1D("hPredNTops",title+";N_{tops};Events",5, 0, 5);
   hPredNTops->Sumw2();
-  hPredMT2 = new TH1D("hPredMT2",title+";M_{T2}[GeV];Events",50,0,500);
+  hPredMT2 = new TH1D("hPredMT2",title+";M_{T2}[GeV];Events",20,0,500);
   hPredMT2->Sumw2();
   hPredmTcomb = new TH1D("hPredmTcomb",title+";M_{Tb}+0.5*M_{Tt}[GeV];Events",50,0,1000);
   hPredmTcomb->Sumw2();
 
   hTrueHt = new TH1D("hTrueHt",title+";H_{T} [GeV];Events",25,200.,2000.);
   hTrueHt->Sumw2();
-  hTruemet = new TH1D("hTruemet",title+";met [GeV];Events",50,200.,1200.);
+  hTruemet = new TH1D("hTruemet",title+";met [GeV];Events",24,200.,800.);
   hTruemet->Sumw2();
   hTrueNJets = new TH1D("hTrueNJets",title+";N_{jets};Events",10,4,14);
   hTrueNJets->Sumw2();
@@ -67,7 +67,7 @@ void BaseHistgram::BookHistgram(const char *outFileName)
   hTrueNbJets->Sumw2();
   hTrueNTops = new TH1D("hTrueNTops",title+";N_{tops};Events",5, 0, 5);
   hTrueNTops->Sumw2();
-  hTrueMT2 = new TH1D("hTrueMT2",title+";M_{T2}[GeV];Events",50,0,500);
+  hTrueMT2 = new TH1D("hTrueMT2",title+";M_{T2}[GeV];Events",20,0,500);
   hTrueMT2->Sumw2();
   hTruemTcomb = new TH1D("hTruemTcomb",title+";M_{Tb}+0.5*M_{Tt}[GeV];Events",50,0,1000);
   hTruemTcomb->Sumw2();
@@ -100,4 +100,31 @@ double mhtJetEtaMax() {return 5;}
 double nJetPtMin(){return 30;}
 double nJetEtaMax() {return 2.4;}
 
+void drawOverFlowBin(TH1 *hist){
 
+  int nbins = hist->GetXaxis()->GetNbins();
+
+  double overflow = hist->GetBinContent(nbins+1);
+  double lastCont = hist->GetBinContent(nbins);
+  double ovrflweroor = hist->GetBinError(nbins+1);
+  double lstbinerror = hist->GetBinError(nbins);
+  hist->SetBinContent(nbins, overflow+lastCont);
+  hist->SetBinError(nbins, TMath::Sqrt(ovrflweroor * ovrflweroor + lstbinerror * lstbinerror));
+}
+double calcMT(const TLorentzVector &objLVec, const TLorentzVector &metLVec){
+
+
+
+  const double objMass = objLVec.M(), objPt = objLVec.Pt(), objPx = objLVec.Px(), objPy = objLVec.Py();
+
+  const double met = metLVec.Pt(), metphi = metLVec.Phi();
+
+
+
+  double mt = sqrt( objMass*objMass + 2*( met*sqrt(objMass*objMass + objPt*objPt) -( met*cos(metphi)*objPx + met*sin(metphi)*objPy ) ) );
+
+
+
+  return mt;
+
+}
